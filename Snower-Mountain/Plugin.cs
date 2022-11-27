1955ae77dcfc7b7291c5a24a5e2a086154d3de9f
Administrator <Administrator@GRAZE>
2022-11-27 22:37:39 +0000

    Add project files.

---
+++ Snower-Mountain/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using Utilla;
+namespace Snower_Mountain
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        GameObject snowbundle;
+        void Start()
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("Snower-Mountain.Assets.snowbundle");
+            AssetBundle bundle = AssetBundle.LoadFromStream(str);
+            GameObject powder = bundle.LoadAsset<GameObject>("snowbundle");
+            snowbundle = Instantiate(powder);
+            GameObject.Find("Level/mountain/Geometry/mountainside").GetComponent<MeshRenderer>().material = GameObject.Find("snowbundle(Clone)/3D snow").GetComponent<Renderer>().material;
+            GameObject.Find("snowbundle(Clone)").transform.SetParent(GameObject.Find("Level/mountain").transform, true);
+        }
+
+    }
+}
---
+++ Snower-Mountain/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Snower_Mountain
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.snower-mountain";
+        public const string Name = "Snower_Mountain";
+        public const string Version = "1.0.0";
+    }
+}
